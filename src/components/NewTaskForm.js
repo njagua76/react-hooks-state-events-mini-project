e9b4d0303892ffc159bf7d34f1ff744a575f3b83
Ann Gathoni <theanngathoni@gmail.com>
2025-08-25 01:08:25 +0300

    created a task lister using states and events

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("Code");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onTaskFormSubmit({ text, category });
+    setText("");
+    setCategory("Code");
+  }
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
+    <form className="new-task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="text"
+        placeholder="New task details"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <select
+        name="category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        {categories
+          .filter((c) => c !== "All")
+          .map((c) => (
+            <option key={c} value={c}>
+              {c}
+            </option>
+          ))}
+      </select>
       <input type="submit" value="Add task" />
     </form>
   );
 }
 
 export default NewTaskForm;
+
