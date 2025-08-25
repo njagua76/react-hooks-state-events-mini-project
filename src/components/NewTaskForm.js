ebc742536ee12d55ae2af2d3cc9f68d9b410a923
Ann Gathoni <theanngathoni@gmail.com>
2025-08-25 12:43:17 +0300

    the errors

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -13,28 +13,33 @@ function NewTaskForm({ categories, onTaskFormSubmit }) {
 
   return (
     <form className="new-task-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="text"
-        placeholder="New task details"
-        onChange={(e) => setText(e.target.value)}
-
-      />
-      <select
-      name = "category"
-      value ={category}
-      onChange={(e) => setCategory(e.target.value)}>
-        {categories
-          .filter((c) => c !== "All")
-          .map((c) => (
-            <option key={c} value={c}>
-              {c}
-            </option>
-          ))}
-      </select>
+      <label>
+        Details
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="New task details"
+        />
+      </label>
+      <label>
+        Category
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          {categories
+            .filter((c) => c !== "All")
+            .map((c) => (
+              <option key={c} value={c}>
+                {c}
+              </option>
+            ))}
+        </select>
+      </label>
       <button type="submit">Add task</button>
     </form>
   );
 }
-      
+
 export default NewTaskForm;
